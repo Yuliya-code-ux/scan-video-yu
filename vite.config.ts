9f90fed6046a2ea0ff175642fe2506c7f1c780c7
Yuliya-code-ux <xasinaeva@inbox.ru>
2026-06-04 05:25:27 +0300

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/scan-video-yu/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
